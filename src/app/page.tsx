651d646b7c4d19501511249a21b9481b4c6840e3
BohdanVoznyi <bohdanvoznyi.em@gmail.com>
2025-01-05 11:20:12 +0200

    linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
                         </code>
                         .
                     </li>
-                    <li>Save and see your changes instantly.</li>
+                    <li>Save and see your changes instantly. Test changes</li>
                 </ol>
 
                 <div className="flex gap-4 items-center flex-col sm:flex-row">
